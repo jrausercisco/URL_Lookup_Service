e7817268caa85f09a11cdb10a7dde09e7d3db3a2
Tim Potteiger <tpottei1@umbc.edu>
2020-10-24 22:42:40 -0400

    simplified api and added url query endpoint

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import List, Optional
 
 import databases
 import sqlalchemy
+from sqlalchemy import select
+from sqlalchemy import delete
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -20,24 +22,23 @@ from pydantic import BaseModel
 
 
 
-# SQLAlchemy specific code, as with any other app
 DATABASE_URL = "sqlite:///./test.db"
-# DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 database = databases.Database(DATABASE_URL)
 
 metadata = sqlalchemy.MetaData()
 
-bulk_requests_table = sqlalchemy.Table(
-    "bulk_requests",
+
+category_mapping_table = sqlalchemy.Table(
+    "category_mapping",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("url_requests", sqlalchemy.String),
-    sqlalchemy.Column("all_resolved", sqlalchemy.Boolean),
+    sqlalchemy.Column("url", sqlalchemy.String),
+    sqlalchemy.Column("category", sqlalchemy.String),
 )
 
-categories_table = sqlalchemy.Table(
-    "categories",
+pending_resolved_table = sqlalchemy.Table(
+    "pending_resolved",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column("url", sqlalchemy.String),
@@ -73,31 +74,42 @@ async def startup():
 async def shutdown():
     await database.disconnect()
 
-
-@app.get("/bulk_requests/")
-async def read_bulk_requests():
-    query = bulk_requests_table.select()
+@app.get("/url_category_mapping/")
+async def read_category_mapping():
+    query = category_mapping_table.select()
     return await database.fetch_all(query)
 
-
-@app.post("/bulk_requests/")
-async def create_bulk_request(bulk: List[str]):
-    str1 = " "
-    query = bulk_requests_table.insert().values(url_requests=str1.join(bulk), all_resolved=False)
-    last_record_id = await database.execute(query)
-    return {"id": last_record_id}
-
-@app.get("/categories/")
-async def read_categories():
-    query = categories_table.select()
+@app.get("/pending_resolved_url_category_mapping/")
+async def read_pending_resolved():
+    query = pending_resolved_table.select()
     return await database.fetch_all(query)
 
+@app.get("/url_query/")
+async def url_query(url_str: str):
+    query = select([category_mapping_table]).where(category_mapping_table.c.url == url_str)
+    result = await database.fetch_one(query)
+    if result is None:
+        pending_query = select([pending_resolved_table]).where(pending_resolved_table.c.url == url_str)
+        pending_result = await database.fetch_one(pending_query)
+        if pending_result is None:
+            inserted = pending_resolved_table.insert().values(url=url_str)
+            last_record_id = await database.execute(inserted)
+    return result
 
-@app.post("/categories/")
-async def create_category_submit(bulk: List[URLItem]):
+@app.post("/url_category_mapping/")
+async def create_category_mapping_submit(bulk: List[URLItem]):
+    last_record_id = -1
     for item in bulk:
-        query = categories_table.insert().values(url=item.url, category=item.category)
-        last_record_id = await database.execute(query)
+        does_exist = select([category_mapping_table]).where(category_mapping_table.c.url == item.url)
+        result_exist = await database.fetch_one(does_exist)
+        if result_exist is None:
+            query = category_mapping_table.insert().values(url=item.url, category=item.category)
+            last_record_id = await database.execute(query)
+            is_pending = select([pending_resolved_table]).where(pending_resolved_table.c.url == item.url)
+            result_for_pending = await database.fetch_one(is_pending)
+            if result_for_pending is not None:
+                to_delete = pending_resolved_table.delete().where(pending_resolved_table.c.url == item.url)
+                result_for_delete = await database.fetch_one(to_delete)
     return {"id": last_record_id}
 
 
